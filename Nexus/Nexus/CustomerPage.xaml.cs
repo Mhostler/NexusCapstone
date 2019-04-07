c362fa1c4c72df63a319d34617d1437e85a64e46
Saujanya17 <smani@unomaha.edu>
2019-04-07 18:44:15 -0500

    Customer and Vendor Insertion Done

--- Nexus/Nexus/CustomerPage.xaml.cs
+++ Nexus/Nexus/CustomerPage.xaml.cs
@@ -34,5 +34,44 @@ namespace Nexus
             Uri uri = new Uri("HomePage.xaml", UriKind.Relative);
             this.NavigationService.Navigate(uri);
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Customer c = new Customer();
+            c.Id = Int32.Parse(IdBox.Text);
+            c.Phone = PhoneBox.Text;
+            c.Email = EmailBox.Text;
+            //List.GetValue(c.Id);
+            DataGrid grid = new DataGrid();
+            grid.FrozenColumnCount = 5;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Customer c = new Customer();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Uri u = new Uri("CustomerInsertPage.xaml", UriKind.Relative);
+            this.NavigationService.Navigate(u);
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid grid = new DataGrid();
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Customer c = new Customer();
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            Customer x = new Customer();
+        }
     }
 }
