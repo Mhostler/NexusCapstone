33b610c5b0696cf9528721c7c3f0048f72fe22a0
Mike <mhostler@unomaha.edu>
2019-04-23 20:28:40 -0500

    added get open orders

--- Nexus/Nexus/DBHandler.cs
+++ Nexus/Nexus/DBHandler.cs
@@ -297,6 +297,78 @@ namespace Nexus
             return oList;
         }
 
+        public static List<Order> getOpenOrders()
+        {
+            List<Order> oList = new List<Order>();
+            string query = "Select * from Orders WHERE Received='0001-01-01'";
+
+            if (OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Order o = new Order
+                    {
+                        OrderID = Int32.Parse(reader["OrderID"] + ""),
+                        Placed = DateTime.Parse(reader["Placed"] + ""),
+                        Received = DateTime.Parse(reader["Received"] + "")
+                    };
+
+                    oList.Add(o);
+                }
+
+                for (int i = 0; i < oList.Count; i++)
+                {
+                    reader.Close();
+
+                    query = "select " +
+                        "OrderItems.oID as oID, " +
+                        "OrderItems.Quantity as quant, " +
+                        "OrderItems.vmID as vmID, " +
+                        "VendorMerch.VendorID as VendorID, " +
+                        "VendorMerch.ItemID as ItemID, " +
+                        "VendorMerch.UnitSize as UnitSize, " +
+                        "VendorMerch.UnitPrice as UnitPrice, " +
+                        "Merch.Name as Name, " +
+                        "Merch.Size as Size, " +
+                        "Merch.Inventory as Inv, " +
+                        "Merch.Price as Price " +
+                        "FROM ((" +
+                        "OrderItems INNER JOIN VendorMerch on OrderItems.vmID=VendorMerch.vmID) " +
+                        "INNER JOIN Merch on VendorMerch.ItemID = Merch.ItemID) " +
+                        "WHERE OrderItems.OrderID=" + oList[i].OrderID.ToString();
+                    cmd.CommandText = query;
+
+                    reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Merchandise m = new Merchandise
+                        {
+                            Name = reader["Name"] + "",
+                            Size = reader["Size"] + "",
+                            Inventory = Int32.Parse(reader["Inv"] + ""),
+                            Price = decimal.Parse(reader["Price"] + "")
+                        };
+                        VendorItem vi = new VendorItem(m,
+                            Int32.Parse(reader["vmID"] + ""),
+                            Int32.Parse(reader["UnitSize"] + ""),
+                            decimal.Parse(reader["UnitPrice"] + ""),
+                            Int32.Parse(reader["VendorID"] + ""));
+
+                        oList[i].addItem(vi, Int32.Parse(reader["quant"] + ""));
+                    }
+                }
+
+                reader.Close();
+                CloseConnection();
+            }
+
+            return oList;
+        }
+
         public static List<Order> getOrderByRange(DateTime start, DateTime end)
         {
             List<Order> oList = new List<Order>();
